381d37583a078c3659c422ee1fa61fcb5c4ab789
Alex <j@dytry.ch>
2016-07-04 18:25:00 +0100

    Add basic support for stateless components

---
+++ src/__test__/fixtures/stateless.input.js
@@ -0,0 +1,27 @@
+var React = require('react');
+
+type FooProps = {
+  an_optional_string?: string,
+  a_number: number,
+  a_boolean: boolean,
+  a_generic_object: Object,
+  array_of_strings: Array<string>,
+  instance_of_Bar: Bar,
+  anything: any,
+  one_of: 'QUACK' | 'BARK' | 5,
+  onw_of_type: number | string,
+  nested_object_level_1: {
+    string_property_1: string,
+    nested_object_level_2: {
+      nested_object_level_3: {
+        string_property_3: string,
+      },
+      string_property_2: string,
+    }
+  },
+  should_error_if_provided: void
+}
+
+export default function Foo(props: FooProps) {
+
+}
