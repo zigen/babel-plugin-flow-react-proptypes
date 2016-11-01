576667728e19172884098a25db90f6bafe2e9fe0
Frankie Bagnardi <f.bagnardi@gmail.com>
2016-11-01 12:48:35 -0700

    allows forcing updates of tests with UPDATE envvar

--- test/index.js
+++ test/index.js
@@ -5,6 +5,8 @@ const assert = require('assert');
 const babel  = require('babel-core');
 const plugin = require('../src/index').default;
 
+const UPDATE = !!process.env.UPDATE;
+
 function trim(str) {
   return str.replace(/^\s+|\s+$/, '');
 }
@@ -32,6 +34,11 @@ describe('emit asserts for: ', () => {
         let expected;
         try {
           expected = fs.readFileSync(path.join(fixturesDir, name + OUTPUT_POSTFIX)).toString();
+          if (expected.trim() !== actual.trim() && UPDATE) {
+            console.error('Updating for fixture ' + name);
+            console.error(actual);
+            throw new Error('just to skip to the catch');
+          }
         }
         catch (e) {
           expected = actual;
