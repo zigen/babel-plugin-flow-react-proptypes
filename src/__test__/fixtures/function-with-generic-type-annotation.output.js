67e56f4d399fe4c19235201c5c7864fdef4d8640
Frankie Bagnardi <f.bagnardi@gmail.com>
2016-11-03 12:56:01 -0700

    fixes tests

--- src/__test__/fixtures/function-with-generic-type-annotation.output.js
+++ src/__test__/fixtures/function-with-generic-type-annotation.output.js
@@ -1,7 +1,6 @@
 "use strict";
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-exports.default = function (props) {};
+var C = function C(props) {};
+C.propTypes = {
+  name: require("react").PropTypes.string.isRequired
+};
