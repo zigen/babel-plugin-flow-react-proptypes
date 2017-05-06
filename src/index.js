72f7c409d45f0029a9936f272d0f62635c42e1ed
Ryan Leckey <ryan@launchbadge.com>
2017-05-06 15:55:24 -0700

    Add a babel option ignoreNodeModules to control transform on node modules

--- src/index.js
+++ src/index.js
@@ -124,7 +124,7 @@ module.exports = function flowReactPropTypes(babel) {
 
   return {
     visitor: {
-      Program(path) {
+      Program(path, {opts}) {
         internalTypes = {};
         importedTypes = {};
         suppress = false;
@@ -137,8 +137,9 @@ module.exports = function flowReactPropTypes(babel) {
         }
         if (this.file && this.file.opts && this.file.opts.filename) {
           if (this.file.opts.filename.indexOf('node_modules') >= 0) {
-            // Suppress any file that lives in node_modules
-            suppress = true;
+            // Suppress any file that lives in node_modules IF the
+            // ignoreNodeModules setting is true
+            suppress = opts.ignoreNodeModules;
           }
         }
       },
