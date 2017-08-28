1a29c562f947289d1fa57a1f7402f62a39a020d3
Frankie Bagnardi <f.bagnardi@gmail.com>
2017-08-28 05:55:36 -0700

    adds class-single-type-test

---
+++ src/__tests__/class-single-type-test.js
@@ -0,0 +1,21 @@
+const babel = require('babel-core');
+const content = `
+var React = require('react');
+
+type FooProps = {
+  a_number: number,
+}
+
+export default class Foo extends React.Component<FooProps> {
+  props: FooProps
+}
+`;
+
+it('class-single-type-test', () => {
+  const res = babel.transform(content, {
+    babelrc: false,
+    presets: ['es2015', 'stage-1', 'react'],
+    plugins: ['syntax-flow', require('../')],
+  }).code;
+  expect(res).toMatchSnapshot();
+});
